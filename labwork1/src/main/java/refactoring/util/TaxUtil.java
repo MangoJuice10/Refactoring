7b943fe4d50c73c7fe0011e0a6aec2d47e6cb96f
MangoJuice10 <swordmaster10A@gmail.com>
2025-10-12 17:21:39 +0300

    Refactor TaxUtil

--- labwork1/src/main/java/refactoring/util/TaxUtil.java
+++ labwork1/src/main/java/refactoring/util/TaxUtil.java
@@ -1,16 +1,11 @@
 package refactoring.util;
 
 public class TaxUtil {
-    // REFACTORING CANDIDATE: Remove Assignments to Parameters (Происходит
-    // присвоение параметру amount нового значения)
     public double calcTax(double amount, double rate) {
-        // REFACTORING CANDIDATE: Inline Temp (Переменная constant не несёт смысловой
-        // нагрузки, поэтому можно встроить её значение в связанное выражение)
-        double constant = 1.0;
-        amount = amount + constant;
+        double taxBase = amount + 1.0;
         if (rate < 0) {
             rate = 0;
         }
-        return amount * rate;
+        return taxBase * rate;
     }
 }
