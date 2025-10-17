f4975a6cfe26f8303f6bfc5641e551f939e73d16
MangoJuice10 <swordmaster10A@gmail.com>
2025-10-17 14:25:48 +0300

    Refactor the code with Replace Nested Conditional with Guard Clauses

--- Lab3/labwork3/src/main/java/refactoring/RefundProcessor.java
+++ Lab3/labwork3/src/main/java/refactoring/RefundProcessor.java
@@ -1,30 +1,23 @@
 package refactoring;
 
 public class RefundProcessor {
-
-    // REFACTORING NEEDED: Replace Nested Conditional with Guard
-    // Clauses (Слишком много вложенных условных операторов, которые следует
-    // заменить граничными операторами)
     public double calculateRefund(Reservation reservation, long currentEpochSeconds) {
-        if (reservation.isCanceled()) {
-            if (reservation.getPaidAtEpochSeconds() > 0) {
-                long secondsSincePaid = currentEpochSeconds - reservation.getPaidAtEpochSeconds();
-                if (secondsSincePaid <= 24 * 3600) {
-                    Customer c = reservation.getCustomer();
-                    if (c.getName() != null && c.getName().startsWith("VIP")) {
-                        return computeAmount(reservation);
-                    } else {
-                        return computeAmount(reservation) * 0.5;
-                    }
-                } else {
-                    return 0.0;
-                }
-            } else {
-                return 0.0;
-            }
-        } else {
+        if (!reservation.isCanceled()) {
             return 0.0;
         }
+        if (!(reservation.getPaidAtEpochSeconds() > 0)) {
+            return 0.0;
+        }
+        long secondsSincePaid = currentEpochSeconds - reservation.getPaidAtEpochSeconds();
+        if (!(secondsSincePaid <= 24 * 3600)) {
+            return 0.0;
+        }
+
+        Customer c = reservation.getCustomer();
+        if (c.getName() != null && c.getName().startsWith("VIP")) {
+            return computeAmount(reservation);
+        }
+        return computeAmount(reservation) * 0.5;
     }
 
     private double computeAmount(Reservation reservation) {
