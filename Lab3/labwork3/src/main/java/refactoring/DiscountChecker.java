4922fc8bd9488c80dd5e6fe5f1b3158d59fd961f
MangoJuice10 <swordmaster10A@gmail.com>
2025-10-17 12:35:34 +0300

    Refactor the code with Consolidate Conditional Expression

--- Lab3/labwork3/src/main/java/refactoring/DiscountChecker.java
+++ Lab3/labwork3/src/main/java/refactoring/DiscountChecker.java
@@ -1,26 +1,27 @@
 package refactoring;
 
 public class DiscountChecker {
+    private boolean isLoyalCustomer(Customer c) {
+        return c.getLoyaltyPoints() != null && c.getLoyaltyPoints() > 1000;
+    }
 
-    // REFACTORING NEEDED: Consolidate Conditional Expression (несколько условных
-    // операторов ведут к одному и тому же результату, следует объединить их в один
-    // условный оператор)
+    private boolean isCorporateCustomer(Customer c) {
+        return c.getName() != null && c.getName().startsWith("CORP");
+    }
+
+    private boolean isReservationOverTwoMonths(Reservation r) {
+        return r.getDays() > 60;
+    }
+
+    private boolean hasPromotionalEmail(Customer c) {
+        return c.getEmail() != null && c.getEmail().contains("promo20");
+    }
+
     public boolean isEligibleForSpecialDiscount(Reservation reservation) {
         Customer c = reservation.getCustomer();
 
-        if (c.getLoyaltyPoints() != null && c.getLoyaltyPoints() > 1000) {
-            return true;
-        }
-
-        if (c.getName() != null && c.getName().startsWith("CORP")) {
-            return true;
-        }
-
-        if (reservation.getDays() > 60) {
-            return true;
-        }
-
-        if (c.getEmail() != null && c.getEmail().contains("promo20")) {
+        if (isLoyalCustomer(c) || isCorporateCustomer(c) || isReservationOverTwoMonths(reservation)
+                || hasPromotionalEmail(c)) {
             return true;
         }
 
