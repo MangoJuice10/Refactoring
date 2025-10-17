d90a4c3b85ca49d3a49139c97ae95dce1aaf6da9
MangoJuice10 <swordmaster10A@gmail.com>
2025-10-17 13:47:58 +0300

    Refactor the code with Remove Control Flag

--- Lab3/labwork3/src/main/java/refactoring/BookingProcessor.java
+++ Lab3/labwork3/src/main/java/refactoring/BookingProcessor.java
@@ -3,26 +3,21 @@ package refactoring;
 import java.util.List;
 
 public class BookingProcessor {
+    private boolean isCardSedan(Car c) {
+        return c.getType().name().equals("SEDAN");
+    }
 
-    // REFACTORING NEEDED: Remove Control Flag, Replace Nested Conditional with Guard
-    // Clauses (Используется управляющий флаг found, который следует заменить
-    // выходом из цикла с помощью управляющго оператора break; слишком много
-    // вложенных условных операторов, которые следует заменить граничными
-    // операторами)
+    private boolean isCardCompact(Car c) {
+        return c.getType().name().equals("COMPACT");
+    }
+
     public Reservation tryBook(List<Car> fleet, Customer customer, int days) {
-        boolean found = false;
         Reservation result = null;
         for (Car c : fleet) {
-            if (!found) {
-                if (c.isAvailable()) {
-                    if (c.getBasePricePerDay() <= 100.0) {
-                        if (c.getType() == VehicleType.SEDAN || c.getType() == VehicleType.COMPACT) {
-                            c.setAvailable(false);
-                            result = new Reservation("RES-" + c.getId(), c, customer, days, false, 0L, 0L);
-                            found = true;
-                        }
-                    }
-                }
+            if (c.isAvailable() && c.getBasePricePerDay() <= 100.0 && (isCardSedan(c) || isCardCompact(c))) {
+                c.setAvailable(false);
+                result = new Reservation("RES-" + c.getId(), c, customer, days, false, 0L, 0L);
+                break;
             }
         }
 
