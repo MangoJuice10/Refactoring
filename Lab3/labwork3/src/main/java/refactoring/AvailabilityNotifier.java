93685c7ea2576f2d8f036896dbbc3648dcafc6b6
MangoJuice10 <swordmaster10A@gmail.com>
2025-10-17 12:46:48 +0300

    Refactor the code with Consolidate Duplicate Conditional Fragments

--- Lab3/labwork3/src/main/java/refactoring/AvailabilityNotifier.java
+++ Lab3/labwork3/src/main/java/refactoring/AvailabilityNotifier.java
@@ -1,23 +1,17 @@
 package refactoring;
 
 public class AvailabilityNotifier {
-
-    // RFECATORING NEEDED: Consolidate Duplicate Conditional Fragments (две
-    // последние строчки в условных блоках if и else дублируются, следует вынести их
-    // после условных операторов, т.к. в каждом блоке они находятся в конце)
     public String notifyAvailability(Reservation reservation) {
         Car car = reservation.getCar();
         String message;
         if (car.isAvailable()) {
             message = "Car " + car.getId() + " is available for reservation " + reservation.getId();
-            sendEmail(reservation, message);
-            return message;
         } else {
             message = "Car " + car.getId() + " is NOT available for reservation " + reservation.getId()
                     + ". We'll notify when available.";
-            sendEmail(reservation, message);
-            return message;
         }
+        sendEmail(reservation, message);
+        return message;
     }
 
     // Примитивная имитация отправки
